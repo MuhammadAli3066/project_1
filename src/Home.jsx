b7b0189b463b0534a6d22079d91163d2b87bf138
Ali Khan <www.alikhan3066@gmail.com>
2024-07-15 12:26:21 +0500

    updated again

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 function Home() {
   return (
     <>
-      <div class="flex space-x-4 justify-center bg-green-400">
+      <div class="flex space-x-4 justify-center bg-purple-400">
         <div class="bg-blue-500 text-white p-4">Item 1</div>
         <div class="bg-green-500 text-white p-4">Item 2</div>
         <div class="bg-red-500 text-white p-4">Item 3</div>
