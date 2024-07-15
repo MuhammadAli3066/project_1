8690ef33bc5906b8eaaee2c565ac2f32ed717890
Ali Khan <www.alikhan3066@gmail.com>
2024-07-15 12:20:03 +0500

    update background color

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,15 +3,13 @@ import ReactDom from "react-dom";
 function Home() {
   return (
     <>
-      <div class="flex space-x-4 justify-center">
-    <div class="bg-blue-500 text-white p-4">Item 1</div>
-    <div class="bg-green-500 text-white p-4">Item 2</div>
-    <div class="bg-red-500 text-white p-4">Item 3</div>
-  </div>
-
-      
+      <div class="flex space-x-4 justify-center bg-green-400">
+        <div class="bg-blue-500 text-white p-4">Item 1</div>
+        <div class="bg-green-500 text-white p-4">Item 2</div>
+        <div class="bg-red-500 text-white p-4">Item 3</div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home;
